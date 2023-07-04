f7553ccf4b3daefd89993ce94338394e84b7c8bb
Antoine Vandermeersch <avdmeers@gmail.com>
2023-07-04 15:35:44 +0200

    bugfix

--- lib/backend/app/app/gateway/__init__.py
+++ lib/backend/app/app/gateway/__init__.py
@@ -1,8 +1,9 @@
-from .base import NoSuchGame, NoSuchQuestion
+from .base import NoSuchGame, NoSuchQuestion, UnknownToken
 from .dynamo import DynamoGateway
 
 __all__ = [
     "DynamoGateway",
     "NoSuchGame",
     "NoSuchQuestion",
+    "UnknownToken",
 ]
